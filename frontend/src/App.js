233e031f81cfbd8db5188a040d35c004c26c78a4
watercrab <114502369+watercrab@users.noreply.github.com>
2023-02-09 10:50:05 +0700

    my changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Register from "./component/Register";
 import Main from "./component/Main";
+import History from "./component/History";
+import Game from "./component/Game";
+// import game from "./component/game";
+// import history from "./component/history";
 function App() {
   return (
-    <div className="App">
-      {/* <Register /> */}
-      <Main/>
-    </div>
+    // <div className="App">
+    //   {/* <Register /> */}
+    //   <Main/>
+    //   {/* <game/>
+    //   <history/> */}
+    // </div>
+    <Routes>
+      <Route path="/Main" element={<Main />} />
+      <Route path="/Game" element={<Game />} /> 
+      <Route path="/history" element={<History/>} />
+    </Routes>
   );
 }
 
--- frontend/src/component/Main.jsx
+++ frontend/src/component/Main.jsx
@@ -1,58 +1,78 @@
-import React from 'react'
-import "./Main.css"
-import profile from "../img/profile.png"
-import tab from "../img/bg-tab.png"
-import bg from "../img/background.jpeg"
-import bird from "../img/bird.jpeg"
-import btnAdd from "../img/btnAdd.png"
-import btnPlay from "../img/btnPlay.png"
-import btnTest from "../img/btnTest.png"
-import cactus from "../img/cactus.png"
-import fish from "../img/fish.jpeg"
-import history from "../img/history.png"
-import language from "../img/language.png"
-import logout from "../img/log-out.png"
-// import mushroom from "../mushroom.png"
-import panguin from "../img/panguin.png"
-import popo from "../img/popo.jpg"
-import setting from "../img/setting.png"
-import snow from "../img/snow.png"
-import sound from "../img/snow.png"
-import treasury from "../img/treasury.png"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Main.css";
+import "./Main";
+import "./Game";
+import "./History";
+import profile from "../img/profile.png";
+import tab from "../img/bg-tab.png";
+import btnAdd from "../img/btnAdd.png";
+import btnPlay from "../img/btnPlay.png";
+import btnTest from "../img/btnTest.png";
+import history from "../img/history.png";
+import language from "../img/language.png";
+import logout from "../img/log-out.png";
+import setting from "../img/setting.png";
+import sound from "../img/sound01.png";
+import treasury from "../img/treasury.png";
 
 function Main() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div id="root">
-        <h1 class="main-page">User name</h1>
-        <p class="main-page" id="point">point : 100 </p>
-        <img src={profile} id="profile" />
-        {/* <div  className="GeeksForGeeks" ></div> */}
-        <div className='bg-tab'></div>
-        {/* <div id="lol" src={popo}></div> */}
+      <div id="main">
 
-        <div class="tab">
-          <img id="bg-tab" src={tab} weight="50px" height="500px"/> 
-          <img id="treasury" src={treasury} weight="50px" height="50px"/>
-          <p class="tab-text" id="treasury-text">คลัง</p>
-          <img id="history" src={history} weight="50px" height="50px"/>
-          <p class="tab-text" id="history-text">ประวัติ</p>
-          <img id="setting" src={setting} weight="50px" height="50px"/>
-          <p class="tab-text" id="setting-text">ตั่งค่า</p>
-          <img id="log-out" src={logout} weight="50px" height="50px"/>
-          <p class="tab-text" id="log-out-text">ออกจากระบบ</p>
+        <div className="bg" weight="1500px" height="1500px">
+          <h1 className="main-page">User name</h1>
+          <p className="main-page" id="point">
+            point : 100{" "}
+          </p>
+          <div onClick={() => navigate("/Main")}>
+            <img src={profile} id="profile" />
+          </div>
+
+          <div className="tab">
+            <img id="bg-tab" src={tab} weight="50px" height="500px" />
+            <img id="treasury" src={treasury} weight="50px" height="50px" />
+            
+            <p className="tab-text" id="treasury-text">
+              คลัง
+            </p>
+            <div  id="cursor-his" onClick={() => navigate("/history")} >
+              <img id="history" src={history} weight="50px" height="50px" />
+            </div>
+            <p className="tab-text" id="history-text">
+              ประวัติ
+            </p>
+            <img id="setting" src={setting} weight="50px" height="50px" />
+            <p className="tab-text" id="setting-text">
+              ตั่งค่า
+            </p>
+            <img id="log-out" src={logout} weight="50px" height="50px" />
+            <p className="tab-text" id="log-out-text">
+              ออกจากระบบ
+            </p>
+          </div>
+
+          <div className="center">
+            <div onClick={() => navigate("/Game")}>
+              <img id="play" src={btnPlay} weight="50px" height="500px" />
+            </div>
+            <img id="add" src={btnAdd} weight="50px" height="350px" />
+            <img id="test" src={btnTest} weight="20px" height="300px" />
+          </div>
+
+          <div className="right-bar">
+            <img id="sound" src={sound} weight="40px" height="40px" />
+            <img id="language" src={language} weight="40px" height="40px" />
+          </div>
         </div>
-    
-        <div class="center">
-          <img id="play" src={btnPlay.png} weight="50px" height="500px"/>
-          <img id="add" src={btnAdd.png} weight="50px" height="350px"/>
-          <img id="test" src={btnTest.png} weight="20px" height="300px"/>
-        </div>
+
+        
       </div>
-
-      
-  </>
-  )
+    </>
+  );
 }
 
-export default Main
+export default Main;
